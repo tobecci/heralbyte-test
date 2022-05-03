f2f6467bcacc0b3467aa9a20c4e98ada92614350
tobecci <ojinakatochukwu@gmail.com>
2022-05-03 15:20:45 +0100

    fix: corrected npm scripts and added express packages

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,8 @@
+import express from "express"
+
 console.log("hajimemashite");
+const app = express();
+const port = process.env.PORT || 3000;
+app.listen(port, function(){
+    console.log(`app served on http://localhost:${port}`)
+})
