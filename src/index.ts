29ca34751b630b1344a2839d463a5365b97c548d
tobecci <ojinakatochukwu@gmail.com>
2022-09-06 17:26:15 +0100

    fix: remove sqlite file, format

--- src/index.ts
+++ src/index.ts
@@ -1,28 +1,26 @@
 import express from "express"
 import { initControllers } from "./controllers"
-import { Sequelize, Model, DataTypes } from 'sequelize';
-import { join } from "path"
+import { Sequelize, DataTypes } from 'sequelize';
 
+async function init(){
+    const sequelize = new Sequelize('postgres://hefmyxoh:U_jsxsSYHj4kArZQDwmp1_4eVgzpC1KJ@stampy.db.elephantsql.com/hefmyxoh')
 
-let pathToSqliteFile = join(__dirname, '../db.sqlite');
+    // define user model
+    const User = sequelize.define('User', {
+        target: DataTypes.STRING,
+        network: DataTypes.STRING,
+    });
 
-const sequelize = new Sequelize({
-    dialect: 'sqlite',
-    storage: pathToSqliteFile
-});
+    await sequelize.sync({force: true});
+    
+    const app = express();
+    app.use(express.json());
+    app.use(express.urlencoded({extended: true}));
+    initControllers(app, sequelize);
+    const port = process.env.PORT || 5000;
+    app.listen(port, function(){
+        console.log(`app served on http://localhost:${port}`)
+    })
+}
 
-// define user model
-const User = sequelize.define('User', {
-  target: DataTypes.STRING,
-  network: DataTypes.STRING,
-});
-sequelize.sync({force: true});
-
-const app = express();
-app.use(express.json());
-app.use(express.urlencoded({extended: true}));
-initControllers(app, sequelize);
-const port = process.env.PORT || 5000;
-app.listen(port, function(){
-    console.log(`app served on http://localhost:${port}`)
-})
+init();
